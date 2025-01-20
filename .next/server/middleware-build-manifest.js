66773072dd36dd45af3c44a41939ec7dae9b9dd0
Chelohub <cheloappdev225@gmail.com>
2025-01-20 00:53:49 +0000

    delete 78 files and update 34 files

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,21 @@
-self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-5ae347c2619db9f4.js","static/chunks/fd9d1056-2d3ec73faabe5d64.js","static/chunks/23-c9f58d578b6b0106.js","static/chunks/main-app-c9076d94367c1ca8.js"],pages:{"/_app":["static/chunks/webpack-5ae347c2619db9f4.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-72b1c7c6ed76f479.js","static/chunks/pages/_app-6a626577ffa902a4.js"],"/_error":["static/chunks/webpack-5ae347c2619db9f4.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-72b1c7c6ed76f479.js","static/chunks/pages/_error-1be831200e60c5c0.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+self.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\hp\Desktop\projects\picxob.borislukrece.com\picxob\src\app\page.tsx
+// File: C:\Users\Chelovek\Desktop\website.online\picxob.borislukrece.pro\src\app\page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
